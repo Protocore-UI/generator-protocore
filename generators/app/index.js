c4bf4201b6dc8fafe5dc9c762417daf4b969217a
Ashwin Hegde <ashwinh@cybage.com>
2015-04-23 12:21:50 +0530

    initial methods

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,29 +1,68 @@
 var generators = require('yeoman-generator');
 
-/*
-	Either type of generator can be extended to create an app generator or a subcommand generator. 
-	Base is extended, most often, for an app generator and NamedBase for a subcommand generator
-	where a filename is required.
-*/
-
-// module.exports = generators.Base.extend();
-// module.exports = generators.NamedBase.extend();
-
 module.exports = generators.Base.extend({
-	// The name `constructor` is important here
 	constructor: function() {
 		// Calling the super constructor is important so our generator is correctly set up
 		generators.Base.apply(this, arguments);
 
-		// Next, add your custom code
-		this.option('coffee'); // This method adds support for a `--coffee` flag
+		this.program = require('../../package.json');
+
+		this.option('smacss', {
+			desc: 'Use SMACSS',
+			type: Boolean,
+			defaults: false
+		});
+		this.smacss = this.options.smacss;
 	},
 
-	method1: function() {
-		console.log('method 1 just ran');
+	ui: function() {
+		var prompts = [{
+			type: "list",
+			name: "approach",
+			message: "Do you want scaffolding to include SMACSS approach ? (Default: Yes)",
+			choices: [
+				"Skip this question. Keep the default.",
+				"No"
+			],
+			filter: function(val) {
+				if (val.toLowerCase().indexOf("skip") > -1) {
+					return true;
+				}
+				return false;
+			}
+		}, {
+			type: "list",
+			name: "preprocessor",
+			message: "Do you want scaffolding to include CSS preprocessor ? (Default: CSS)",
+			choices: [
+				"LESS - Configures LESS.",
+				"Skip this question. Keep the default."
+			],
+			filter: function(val) {
+				var val = val.toLowerCase();
+				if (val.indexOf("less") > -1) {
+					return 'less';
+				} else {
+					return false;
+				}
+			}
+		}];
+
+		this.prompt(prompts, function(answers) {
+			console.log(answers);
+		}.bind(this));
 	},
-	
-	method2: function() {
-		console.log('method 2 just ran');
+
+	app: function() {
+		this.mkdir('config');
+		this.mkdir('config/build');
+		this.mkdir('config/lints');
+		this.mkdir('src');
+		this.mkdir('src/apps');
+		this.mkdir('src/assets');
+		this.mkdir('src/stylesheets');
+		this.mkdir('src/systems');
+		this.mkdir('src/templates');
+		this.mkdir('tests');
 	}
 });
